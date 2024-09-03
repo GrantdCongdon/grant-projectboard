c2650f83c22609ab7f890e979a8b3c12c394f52a
GrantdCongdon <grantdcongdon@gmail.com>
2024-09-03 02:13:33 -0400

    Initial commit

---
+++ src/pages/Projects/Projects.tsx
@@ -0,0 +1,20 @@
+import "./Projects.css";
+
+export default function Projects() {
+    return (
+        <div className="container">
+            <div className="fishing-rod">
+                <h2>Colton Fishing Rod</h2>
+                <p></p>
+            </div>
+            <div className="wheelchair">
+                <h2>Motion-Controled Wheelchair</h2>
+                <p></p>
+            </div>
+            <div className="micromouse">
+                <h2>Micromouse</h2>
+                <p></p>
+            </div>
+        </div>
+    );
+}
