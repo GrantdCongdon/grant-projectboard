1a6e54827faa272b6cf20ba782fd8eaa05b125c3
GrantdCongdon <grantdcongdon@gmail.com>
2024-09-03 02:35:15 -0400

    Reconfigured static site

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    distDir: 'build',
+    output: 'export'
+};
 
 export default nextConfig;
